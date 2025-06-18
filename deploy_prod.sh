bfde3a62aaa3c9bda4dc5386ad79b843408bb696
Isa Pasco <isa@localhost.localdomain>
2025-06-18 16:53:21 +0800

    updated kuka

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -5,7 +5,7 @@ set -e  # Exit on error
 # === CONFIGURATION ===
 IMAGE_NAME="ai-civ-web"
 REGISTRY="aicargomation.azurecr.io"
-TAG="v59"
+TAG="v60"
 FULL_IMAGE="$REGISTRY/$IMAGE_NAME:$TAG"
 ACR_NAME="aicargomation"
 
