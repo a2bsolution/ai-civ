78b8de1d51ca4009dd12e354d91d844e508a62c3
james-a2b <dominic.guana@a2bsolutiongroup.com>
2025-07-04 08:44:14 +0800

    updated dev to dev4 civ_jihua_3515_leather_0 and classifier to classify_compile_4

--- deploy_dev.sh
+++ deploy_dev.sh
@@ -5,7 +5,7 @@ set -e  # Exit on error
 # === CONFIGURATION ===
 IMAGE_NAME="ai-civ-web"
 REGISTRY="aicargomation.azurecr.io"
-TAG="dev3"
+TAG="dev4"
 FULL_IMAGE="$REGISTRY/$IMAGE_NAME:$TAG"
 ACR_NAME="aicargomation"
 
